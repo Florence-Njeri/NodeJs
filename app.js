4184348ba0b8f4a3b7d9cd44e6515017617c0777
florence-njeri <flonsie17@gmail.com>
2019-09-26 05:29:32 +0300

    Events and EventEmitter for event driven programming

--- app.js
+++ app.js
@@ -1,2 +1,26 @@
-//A simple js file to test if node js is installed
-console.log('Hello World from Node js');
+//Event module for event driven programming
+const EventEmitter=require('events');
+const eventEmitter=new EventEmitter();
+//Attach a listener to the onject 
+eventEmitter.on('tutorial',(num1,num2)=>{
+    console.log(num1+num2);
+});
+//
+eventEmitter.emit('tutorial',1,2);
+
+//Class extends from the events emitter class
+class Person extends EventEmitter{
+    constructor(name){
+        super();
+        this._name=name;
+    }
+    get name(){
+        return this._name;
+    }
+}
+
+let pedro=new Person('Pedro');
+pedro.on('name',()=>{
+    console.log('My name is '+ pedro.name);
+})
+pedro.emit('name');
